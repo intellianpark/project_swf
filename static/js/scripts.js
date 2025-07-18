db11bd8d89826c53938028e2eb36028bfc678f07
intellianpark <intellian.park@gmail.com>
2025-07-18 12:49:44 +0900

    asdf

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['research-intro', 'people', 'achievements', 'awards']
+const section_names = ['research-intro', 'people', 'achievements', 'progress']
 
 
 window.addEventListener('DOMContentLoaded', event => {
